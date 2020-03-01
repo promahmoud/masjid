e3965a4097b115e90cd3fc456a4b6ccbc9237fa2
promahmoud3@gmail.com <promahmoud3@gmail.com>
2020-03-01 17:52:25 +0200

    update files

--- Demo1/assets/js/custom.js
+++ Demo1/assets/js/custom.js
@@ -138,4 +138,84 @@
     $temp.remove();
   });
 
+  // 
+
+  var fullHeight = function () {
+
+    $('.js-fullheight').css('height', $(window).height());
+    $(window).resize(function () {
+      $('.js-fullheight').css('height', $(window).height());
+    });
+
+  };
+  fullHeight();
+
+  $('#sidebarCollapse').on('click', function () {
+    $('#sidebar').toggleClass('active');
+  });
+
+  // tab 
+
+  $('ul.tabs li').click(function () {
+    var tab_id = $(this).attr('data-tab');
+
+    $('ul.tabs li').removeClass('current');
+    $('.tab-content').removeClass('current');
+
+    $(this).addClass('current');
+    $("#" + tab_id).addClass('current');
+  })
+
+  /*******
+   * ** Contact page
+   */
+  /*==================================================================
+    [ Validate ]*/
+  var input = $('.validate-input .input100');
+
+  $('.validate-form').on('submit', function () {
+    var check = true;
+
+    for (var i = 0; i < input.length; i++) {
+      if (validate(input[i]) == false) {
+        showValidate(input[i]);
+        check = false;
+      }
+    }
+
+    return check;
+  });
+
+
+  $('.validate-form .input100').each(function () {
+    $(this).focus(function () {
+      hideValidate(this);
+    });
+  });
+
+  function validate(input) {
+    if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+      if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+        return false;
+      }
+    } else {
+      if ($(input).val().trim() == '') {
+        return false;
+      }
+    }
+  }
+
+  function showValidate(input) {
+    var thisAlert = $(input).parent();
+
+    $(thisAlert).addClass('alert-validate');
+  }
+
+  function hideValidate(input) {
+    var thisAlert = $(input).parent();
+
+    $(thisAlert).removeClass('alert-validate');
+  }
+
+
 }(jQuery));
